8c8da98ba92bc824988c05820e160d29b0e2125e
Gautam <gautamgohil0@gmail.com>
2024-01-21 18:27:52 +0530

    Made axios instance changes for DEV and PROD environments

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,18 +1,19 @@
 import axiosLibrary from "axios";
+import { APP_ENVIRONMENT, ENVIRONMENT_TYPE } from "../utils/constant.utils";
 
 export const defaultBaseUrl = "http://localhost:3001";
 
-axiosLibrary.defaults.withCredentials = true;
+axiosLibrary.defaults.withCredentials = APP_ENVIRONMENT === ENVIRONMENT_TYPE.DEV ? false : true;
 // create axios interceptor with baseUrl
 const api = axiosLibrary.create({
-    baseURL: defaultBaseUrl
-})
+  baseURL: defaultBaseUrl,
+});
 
-export const createApi = (baseUrl:string) => {
-    return axiosLibrary.create({
-        baseURL: baseUrl || defaultBaseUrl
-        // manage with errors
-    })
-}
+export const createApi = (baseUrl: string) => {
+  return axiosLibrary.create({
+    baseURL: baseUrl || defaultBaseUrl,
+    // manage with errors
+  });
+};
 
-export default api; 
+export default api;
