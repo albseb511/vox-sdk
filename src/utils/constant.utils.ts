ce3512a9b30d4184d96e9b322cd23dde2572d5ef
Gautam <gautamgohil0@gmail.com>
2024-05-29 04:55:23 +0530

    Update on refetch time to 9 minutes

--- src/utils/constant.utils.ts
+++ src/utils/constant.utils.ts
@@ -1,4 +1,4 @@
-export const REFETCH_TOKEN_TIME = 10 * 60 * 1000;
+export const REFETCH_TOKEN_TIME = 9 * 60 * 1000; // 9 minutes in milliseconds ( refetch the token again after 9 minutes )
 export enum ENVIRONMENT_TYPE {
   DEV = "development",
   PROD = "production",
